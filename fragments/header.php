f622b45d70001cd890154ce185bc43705288950b
alicankustemur <alican.kustemur@gmail.com>
2015-09-13 19:52:33 +0300

    font updated, new font Exo 2

--- fragments/header.php
+++ fragments/header.php
@@ -5,26 +5,18 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<link rel="stylesheet" href="css/cosmo/bootstrap.css" />
-		<link rel="stylesheet" href="css/custom.css" />
-		<link rel="stylesheet" href="css/login.css">
 		<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
 		<link rel="stylesheet" href="css/mega-dropdown-menu.css">
 		<link rel="stylesheet" href="css/fm.datetator.jquery.css"/>
 		<link rel="stylesheet" href="css/booking_form.css">
 		<link rel="stylesheet" href="css/jquery.auto-complete.css">
 		<link rel="stylesheet" href="css/first-carousel.css">
-		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/custom.css" />
+	
 		<!-- [if lt IE 9] 
 			<script src="js/html5shiv.js"></script>
 			<script src="js/respond.js"></script>
 		<![endif] -->
-
-		<style>
-			body{
-				font-family: "Source Sans Pro";
-			}
-		</style>
-
 		<title>Tatil Yap</title>
 
 	</head>
