928fc2398b7d7a1b030b24206a62eb342b1083d0
alicankustemur <alican.kustemur@gmail.com>
2015-09-17 02:21:56 +0300

    #update2 updated price list design and added holiday special boxes in footer's bottom

--- fragments/body/holiday_special.php
+++ fragments/body/holiday_special.php
@@ -1,6 +1,5 @@
-<div class="container-fluid" id="holiday-special">
-		
-<div class="container">
+<!-- Holiday Special Start -->
+<div class="container" id="holiday-special">
 	<div class="col-lg-3 col-md-3 col-sm-6">
 		<div class="holiday-special-box">
 			<div class="holiday-special-nested-icon-1">
@@ -21,11 +20,11 @@
 				<i class="fa fa-lock fa-3x"></i>
 			</div>
 			<div class="holiday-special-nested-box-2">Güvenli <br> Alışveriş</div>
-				<div class="holiday-special-box-content">
-					Tatil Yap , SSL sertifikalı ve güvenlik kısmına ağır bir yatırım yapmıştır.
-					Elliot bile bu sisteme giremez ! İddalıyız.Denemekte yarar var deneyin.
-					Tatil Yap'ı tercih ettiğiniz için , teşekkür ederiz.
-				</div>	
+			<div class="holiday-special-box-content">
+				Tatil Yap , SSL sertifikalı ve güvenlik kısmına ağır bir yatırım yapmıştır.
+				Elliot bile bu sisteme giremez ! İddalıyız.Denemekte yarar var deneyin.
+				Tatil Yap'ı tercih ettiğiniz için , teşekkür ederiz.
+			</div>	
 		</div>
 	</div>
 
@@ -35,11 +34,11 @@
 				<i class="fa fa-home fa-3x"></i>
 			</div>
 			<div class="holiday-special-nested-box-3">En İyi <br>Oteller</div>
-				<div class="holiday-special-box-content">
-					Sizlere en iyi hizmeti verebilmek için özenle seçilmiş otelleri 
-					sunuyoruz.Eğer memnun kalmassınız aşağıdaki şikayet bölümünü kullanın.
-					Tatil Yap'ı tercih ettiğiniz için , teşekkür ederiz.
-				</div>	
+			<div class="holiday-special-box-content">
+				Sizlere en iyi hizmeti verebilmek için özenle seçilmiş otelleri 
+				sunuyoruz.Eğer memnun kalmassınız aşağıdaki şikayet bölümünü kullanın.
+				Tatil Yap'ı tercih ettiğiniz için , teşekkür ederiz.
+			</div>	
 		</div>
 	</div>
 
@@ -49,15 +48,13 @@
 				<i class="fa fa-star fa-3x"></i>
 			</div>
 			<div class="holiday-special-nested-box-4">Tatil Yap <br> Farkı</div>
-				<div class="holiday-special-box-content">
-					Fırsat ve kampanyalarımızdan
-					yararlandıkça bizden daha çok memnuniyet duyacağınızı umuyoruz.
-					Hadi durma herkes tatilde sen nerdesin? Tatil Yap'ı tercih ettiğiniz için , teşekkür ederiz.
+			<div class="holiday-special-box-content">
+				Fırsat ve kampanyalarımızdan
+				yararlandıkça bizden daha çok memnuniyet duyacağınızı umuyoruz.
+				Hadi durma herkes tatilde sen nerdesin? Tatil Yap'ı tercih ettiğiniz için , teşekkür ederiz.
 
-				</div>	
+			</div>	
 		</div>
 	</div>
-	
 </div>
-
-</div>
+<!-- Holiday Special End -->
